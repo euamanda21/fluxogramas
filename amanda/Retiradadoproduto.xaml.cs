a8392f873fc3adbbe80423697f40dff23fb32cb5
amanda <amandaleticiaraymundo@gmail.com>
2024-08-06 08:55:57 -0300

    continuou

---
+++ amanda/Retiradadoproduto.xaml.cs
@@ -0,0 +1,19 @@
+using Microsoft.Maui.Controls;
+
+namespace Retiradadoproduto
+{
+    public partial class Retiradadoproduto : ContentPage
+    {
+        public Retiradadoproduto()
+        {
+            InitializeComponent();
+        }
+
+        // Adicione aqui os manipuladores de eventos para os botões, se necessário.
+        // Exemplo:
+        // private void OnButtonClicked(object sender, EventArgs e)
+        // {
+        //     // Código para manipular o evento de clique do botão
+        // }
+    }
+}
---
+++ amanda/Roupasdelazer.xaml.cs
@@ -0,0 +1,20 @@
+using Microsoft.Maui.Controls;
+
+namespace Roupasdelazer
+{
+    public partial class Roupasdelazer
+{ : ContentPage
+    {
+        public Roupasdelazer()
+        {
+            InitializeComponent();
+        }
+
+        // Adicione aqui os manipuladores de eventos para os botões, se necessário.
+        // Exemplo:
+        // private void OnButtonClicked(object sender, EventArgs e)
+        // {
+        //     // Código para manipular o evento de clique do botão
+        // }
+    }
+}
